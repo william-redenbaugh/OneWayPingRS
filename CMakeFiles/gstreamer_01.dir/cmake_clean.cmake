45ebad08d03c1cad533f2540c9f98755042e7590
William Redenbaugh <wredenba@amazon.com>
2022-04-10 12:42:58 -0700

    generated Cpp file and cmake setup for handling gstreamer test latency pipeline

---
+++ CMakeFiles/gstreamer_01.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gstreamer_01.dir/main.cpp.o"
+  "gstreamer_01"
+  "gstreamer_01.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gstreamer_01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
